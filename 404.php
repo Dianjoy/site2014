09ac2c4cbc43f142b711cd5439e5e35b1080aa06
Meathill <meathill@gmail.com>
2013-04-24 18:27:30 +0800

    完成开发者页，开始调试

--- 404.php
+++ 404.php
@@ -5,4 +5,24 @@
  * @author Meathill <lujia.zhai@dianjoy.com>
  * @since 1.0
  */
-?>
+get_header(); ?>
+
+  <div id="primary" class="site-content">
+    <div id="content" role="main">
+
+      <article id="post-0" class="post error404 no-results not-found">
+        <header class="entry-header">
+          <h1 class="entry-title">查无此页</h1>
+        </header>
+
+        <div class="entry-content">
+          <p>您访问的地址似乎有误，没有页面，试试搜索下吧。</p>
+          <?php get_search_form(); ?>
+          <iframe scrolling='no' frameborder='0' src='http://yibo.iyiyun.com/js/yibo404' width='640' height='540'></iframe>
+        </div><!-- .entry-content -->
+      </article><!-- #post-0 -->
+
+    </div><!-- #content -->
+  </div><!-- #primary -->
+
+<?php get_footer(); ?>
--- page.php
+++ page.php
@@ -1,8 +1,32 @@
 <?php
 
 /**
- * @overview 专题
+ * @overview 专题页模版
  * @author Meathill <lujia.zhai@dianjoy.com>
  * @since 1.0
  */
+?>
+<?php
+get_header();
+
+
+if (have_posts()) {
+  the_post();
+  $result = array(
+    'id' => get_the_ID(),
+    'class' => join(' ', get_post_class()),
+    'title' => the_title('', '', FALSE),
+    'content' => str_replace(']]>', ']]&gt;', apply_filters('the_content', get_the_content())),
+    'page' => wp_link_pages(array('before' => '<div class="page-links">' . __('页码：'), 'after' => '</div>', 'echo' => FALSE)),
+  );
+}
+
+require_once('inc/mustache.php');
+$tpl = new Mustache_Engine();
+
+$template = dirname(__FILE__) . '/template/page.html';
+$template = file_get_contents($template);
+echo $tpl->render($template, $result);
+
+get_footer();
 ?>
