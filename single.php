2c39427a095914a7cb309382501efd515c6c6eea
Meathill <meathill@gmail.com>
2013-04-26 12:08:09 +0800

    修复一些问题，将single改成两列

--- single.php
+++ single.php
@@ -12,19 +12,19 @@ get_header(); ?>
 if (have_posts()) {
   while (have_posts()) {
     the_post();
+    $content = get_the_content('继续阅读');
     $blog = array(
       'id' => get_the_ID(),
       'is_featured' => is_sticky() && is_home() && ! is_paged(),
       'class' => join(' ', get_post_class($class, $post_id)),
-      'title' => the_title('', '', FALSE),
       'full_title' => the_title_attribute(array('echo' => FALSE)),
       'is_search' => is_search(),
       'link' => apply_filters('the_permalink', get_permalink()),
       'date' => apply_filters('the_time', get_the_time('Y-m-d'), 'Y-m-d'),
       'excerpt' => apply_filters('the_excerpt', get_the_excerpt()),
-      'content' => the_content('继续阅读'),
-      'thumbnail' => get_the_post_thumbnail(),
-      //'meta' => twentytwelve_entry_meta(),
+      'content' => apply_filters('the_content', $content),
+      'category' => get_the_category_list(' <span class="divider">/</span></li><li>'),
+      'tags' => apply_filters('the_tags', get_the_term_list(0, 'post_tag', '', '，'), '', '，', '', 0),
     );
   }
 }
@@ -34,9 +34,11 @@ $tpl = new Mustache_Engine();
 
 $template = dirname(__FILE__) . '/template/single.html';
 $template = file_get_contents($template);
+echo '<div class="container"><div class="row-fluid">';
 echo $tpl->render($template, $blog);
 
 ?>
 
 <?php get_sidebar(); ?>
+<?php echo '</div></div>' ?>;
 <?php get_footer(); ?>
