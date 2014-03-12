6dfdf2ca202c114e6ece1a00c4f864eced230893
chemzqm <chemzqm@gmail.com>
2014-03-12 18:42:26 +0800

    加入广告墙文档

--- single.php
+++ single.php
@@ -34,7 +34,7 @@ $tpl = new Mustache_Engine();
 
 $template = dirname(__FILE__) . '/template/single.html';
 $template = file_get_contents($template);
-echo '<div class="container"><div class="row-fluid">';
+echo '<div class="grid"><div class="row-fluid">';
 echo $tpl->render($template, $blog);
 
 ?>
