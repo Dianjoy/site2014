164ad524955517d86cbb75c4050269d56c7f29e8
chemzqm <chemzqm@gmail.com>
2014-03-13 18:38:22 +0800

    下载页面调整为后台获取meta

--- download.php
+++ download.php
@@ -13,12 +13,20 @@ get_header();
 
 if (have_posts()) {
   the_post();
+  $meta = get_post_meta( get_the_ID());
+  //echo $meta["version"][0];
+  //var_dump($meta);
+  $id = get_the_ID();
   $result = array(
     'home_url' => esc_url(home_url('/')),
-    'id' => get_the_ID(),
     'class' => join(' ', get_post_class()),
     'title' => the_title('', '', FALSE),
-    'content' => str_replace(']]>', ']]&gt;', apply_filters('the_content', get_the_content()))
+    'content' => str_replace(']]>', ']]&gt;', apply_filters('the_content', get_the_content())),
+    'version' => get_post_meta($id, 'version', true),
+    'description' => get_post_meta($id, 'description', true),
+    'publish_date' => get_post_meta($id, 'publish_date', true),
+    'doc_url' => get_post_meta($id, 'doc_url', true),
+    'download_url' => get_post_meta($id, 'download_url', true),
   );
 }
 require_once('inc/mustache.php');
