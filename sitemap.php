b140080e6bee77e621c5b3bf62d05800e91cfe38
chemzqm <chemzqm@gmail.com>
2014-03-17 17:21:31 +0800

    加入站点地图

---
+++ sitemap.php
@@ -0,0 +1,47 @@
+<?php
+/*
+  Template Name: 网站地图
+ */
+get_header();
+?>
+<div class="grid">
+<h2>站点地图</h2>
+<h3 id="pages">页面</h3>
+<ul>
+<?php
+// Add pages you'd like to exclude in the exclude here
+wp_list_pages(
+  array(
+    'exclude' => '',
+    'title_li' => '',
+  )
+);
+?>
+</ul>
+
+<h3>文章</h3>
+<ul>
+<?php
+// Add categories you'd like to exclude in the exclude here
+$cats = get_categories('exclude=');
+foreach ($cats as $cat) {
+  echo "<li><h3>".$cat->cat_name."</h3>";
+  echo "<ul>";
+  query_posts('posts_per_page=-1&cat='.$cat->cat_ID);
+  while(have_posts()) {
+    the_post();
+    $category = get_the_category();
+    // Only display a post link once, even if it's in multiple categories
+    if ($category[0]->cat_ID == $cat->cat_ID) {
+      echo '<li><a href="'.get_permalink().'">'.get_the_title().'</a></li>';
+    }
+  }
+  echo "</ul>";
+  echo "</li>";
+}
+?>
+</ul>
+</div>
+<?php
+get_footer();
+?>
