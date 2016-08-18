561ec3dad794865c5f3470ce24e76b53bb4fcb67
zombiezlk <zombiezlk@gmail.com>
2016-08-18 16:52:37 +0800

    加上开发者公告

---
+++ dev_posts.php
@@ -0,0 +1,16 @@
+<?php
+define('WP_USE_THEMES', false);
+require('../../../wp-load.php');
+query_posts('showposts=4&tag=gonggao');
+if (have_posts()) {
+  $announcements = array();
+  while (have_posts()) {
+    the_post();
+    $announcements[] = array(
+      'title' => the_title('', '', FALSE),
+      'date' => apply_filters('the_time', get_the_time('m.d'), 'm.d'),
+      'permalink' => apply_filters('the_permalink', get_permalink())
+    );
+  }
+}
+?>
